1600f496238c7ca22fdb865a6795008c4d2ff044
Steven Wiley <s.wiley@katchitek.com>
2021-07-02 16:16:36 +0900

    Added interactive Scheme interpreter back into 'main.c'. Altered version function to act as a simple test. Changed the name of POSIX executable to autoscheme.

--- src/main.c
+++ src/main.c
@@ -4,6 +4,28 @@
 #include "version.h"
 #include "s7.h"
 
+
+int scheme()
+{
+    s7_scheme *s7;
+    char buffer[512];
+    char response[1024];
+
+    s7 = s7_init();              
+
+    while( 1 )                    
+    {
+	fprintf( stdout, "scheme> " );    
+	fgets( buffer, 512, stdin );
+	if(( buffer[0] != '\n' ) || ( strlen(buffer) > 1 ))
+	{
+	    snprintf( response, 1024, "(begin %s)", buffer );
+	    s7_eval_c_string( s7, response ); 
+	}
+    }
+    return 0;
+}
+
 int repl()
 {
     s7_scheme *s7;
@@ -27,14 +49,33 @@ int repl()
 
 int version()
 {
-    printf( "AutoScheme version %s\n", VERSION );
+    /* printf( "AutoScheme version %s\n", VERSION ); */
+
+    s7_scheme *s7;
+    char buffer[512];
+
+    snprintf( buffer, 512, "(begin (display \"AutoScheme version %s\")(newline))", VERSION );
+
+    s7 = s7_init();              
+    s7_eval_c_string( s7, buffer ); 
+ 
     return 0;
 }
 
 int main( int argc, char **argv )
 {
-    /* return repl(); */
-    return version();
+    int i;
+    for( i = 1; i < argc; i++)
+    {
+	if( !strcmp( argv[1], "-V" ))
+	{
+	    return version();
+	}
+	if( !strcmp( argv[1], "-r" ))
+	{
+	    return repl();
+	}
+    }
 
-    return 0;
+    return scheme();
 }

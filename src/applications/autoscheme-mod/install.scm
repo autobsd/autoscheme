b19786d78b3fbaeb02352d3766398b5219baf2c7
Steven Wiley <s.wiley@katchitek.com>
2021-12-30 18:47:38 +0900

    Added error handling for command to install 'autoscheme' in 'autosche-mod' application.

--- src/applications/autoscheme-mod/install.scm
+++ src/applications/autoscheme-mod/install.scm
@@ -51,7 +51,8 @@
 			(error "Make error - unable to build application with module" (string->symbol module)))
 		    (if (not (zero? (process-command install-library-command)))
 			(error "Install error - unable to install library with module" (string->symbol module)))
-		    (process-command install-application-command)
+		    (if (not (zero? (process-command install-application-command)))
+			(error "Install error - unable to install application with module" (string->symbol module)))
 		    (display (directory-files (current-directory)))(newline)
 		    )
 
